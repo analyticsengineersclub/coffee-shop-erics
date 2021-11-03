6aaa8633d89ca4869c4b168e86245956effd3d84
Ubuntu <ubuntu@ip-172-31-16-114.us-east-2.compute.internal>
2021-11-03 20:27:46 +0000

    ADD: add table for ltv calcs

---
+++ models/weekly_customer_orders.sql
@@ -0,0 +1,56 @@
+{{ config(
+    materialized='table'
+) }}
+
+
+with week_spine_base as
+(
+    select
+      *
+    from
+        UNNEST(GENERATE_DATE_ARRAY('2021-01-01', current_date(), INTERVAL 1 week)) AS date_snapshot
+)
+
+, week_spine as
+(
+    select
+      date_trunc(date_snapshot, week) weekly_snapshots
+    from week_spine_base
+)
+
+, purchasing_customers as
+(
+    select
+      distinct customer_id
+    from `analytics-engineers-club.coffee_shop.orders` ord
+)
+
+, customer_week_spine as
+(
+    select
+      week_spine.weekly_snapshots
+    , customer_id
+    , count(*) over (partition by customer_id order by weekly_snapshots asc) week
+    from week_spine
+    cross join `aec-2021-10-thursdays.dbt_cake_robot.customers` customers
+    where week_spine.weekly_snapshots >= cast(date_trunc(customers.min_order_at, week) as date)
+)
+
+, weekly_purchases as
+(
+    select
+      cast(date_trunc(created_at, week) as date) weekly_snapshot
+    , customer_id
+    , sum(total) revenue
+    from `analytics-engineers-club.coffee_shop.orders`
+    group by 1,2
+)
+
+select
+  customer_week_spine.*
+, weekly_purchases.revenue
+, sum(revenue) over (partition by customer_week_spine.customer_id order by customer_week_spine.weekly_snapshots asc) cumulative_revenue
+from customer_week_spine
+left join weekly_purchases 
+on (customer_week_spine.weekly_snapshots = weekly_purchases.weekly_snapshot and 
+    customer_week_spine.customer_id = weekly_purchases.customer_id)
