0f9d1fea863b21c1f3beb992f06bdd5f3dbe4e17
Ubuntu <ubuntu@ip-172-31-16-114.us-east-2.compute.internal>
2021-11-03 18:36:31 +0000

    ADD: add table for pageviews w/ session ids of 30 minute dormancy

---
+++ models/page_views_sessions.sql
@@ -0,0 +1,43 @@
+{{ config(
+    materialized='table'
+) }}
+
+
+with minutes_calc as
+(
+    select
+      pv.*
+    , round(timestamp_diff(timestamp,lag(pv.timestamp) over (partition by visitor_id order by timestamp asc), second) / 60, 2) minutes_diff
+    from `aec-2021-10-thursdays.dbt_cake_robot.page_views_visitor_stitch` pv
+)
+
+, new_session_flag as
+(
+    select
+      minutes_calc.*
+    , case
+        when (minutes_diff > 30 or minutes_diff is null) then 1
+        else 0
+      end as new_session_flag
+    from minutes_calc
+)
+
+, session_id_sum as
+(
+    select
+      nsf.*
+    , sum(new_session_flag) over (partition by visitor_id order by timestamp asc) session_id
+    from new_session_flag nsf
+)
+
+select
+  id
+, visitor_id
+, device_type
+, timestamp
+, page
+, customer_id
+, session_id session_id_num
+, visitor_id || "_" ||session_id visitor_id_session_id
+from session_id_sum
+
